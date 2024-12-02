118bf6b9339e39af6e8f2073dc6432d5c54994f5
AaronML04 <aaronmartinezlopez04@gmail.com>
2024-12-02 19:46:55 +0100

    Add files via upload

---
+++ EDexamen2/src/main/java/com/mycompany/edexamen2/EDexamen2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.edexamen2;
+
+/**
+ *
+ * @author AARON
+ */
+public class EDexamen2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
